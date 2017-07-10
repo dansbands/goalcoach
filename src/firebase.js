e75062ffcad873072050207fdaef29da60ab9e0b
dansbands <dansbands@hotmail.com>
2017-07-10 01:11:53 -0400

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBQPbuU3hkMQYxOm4oD4NZhPtsOcnpBO-c",
+    authDomain: "goalcoach-4dcc6.firebaseapp.com",
+    databaseURL: "https://goalcoach-4dcc6.firebaseio.com",
+    projectId: "goalcoach-4dcc6",
+    storageBucket: "",
+    messagingSenderId: "766586867445"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
+export const goalRef = firebase.database().ref('goals');
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+import { firebaseApp } from './firebase';
+import { logUser } from './actions';
+import reducer from './reducers';
+
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+const history = createHistory();
+const store = createStore(reducer);
+
+
+firebaseApp.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user has signed in or up', user);
+        const { email } = user;
+        store.dispatch(logUser(email));
+        history.push('/app');
+    } else {
+        console.log('user has signed out or still needs to sign in.')
+        history.replace('/signin');
+    }
+})
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router path="/" history={history}>
+            <div>
+                <Route path="/app" component={App} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/signup" component={SignUp} />
+            </div>
+        </Router>
+    </Provider>, document.getElementById('root')
+
+)
