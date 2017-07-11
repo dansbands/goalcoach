9816d9fc63acb29fffe7e9ceed565a1b1d4e97d2
Matt DuLeone <matt@duleone.com>
2017-07-10 20:15:20 -0400

    Add the username to the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,15 +9,30 @@ import CompleteGoalList from './CompleteGoalList';
 
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: (firebaseApp.auth().currentUser && firebaseApp.auth().currentUser.email) || '',
+        };
+
+        if (!firebaseApp.auth().currentUser) {
+            firebaseApp.auth().onAuthStateChanged(user => user && this.updateName(user.email));
+        }
+    }
+
+    updateName(name) {
+        this.setState({name});
+    }
+
     signOut() {
         firebaseApp.auth().signOut();
     }
-
 
     render() {
         return (
             <div style={{margin: '5px'}}>
                 <h3>Goal Coach</h3>
+                <h4>Signed in as <em>{this.state.name}</em></h4>
                 <AddGoal />
                 <hr />
                 <h4>Goals</h4>
